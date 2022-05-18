f743e62a6faaaa00cb723d40443f92ad842c3c19
Floriian <lovasifloriian@outlook.hu>
2022-05-18 17:36:03 +0200

    Server-side body validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
 @Module({
-  imports: [PrismaModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [PrismaModule, ConfigModule.forRoot({ isGlobal: true }), AuthModule],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
@@ -10,6 +11,8 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3000);
 }
 bootstrap();
