83bc095d58546565d96484843f26b2ecbe1bddab
Floriian <lovasifloriian@outlook.hu>
2022-05-18 19:25:39 +0200

    Create post controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,9 @@ import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
 import { PostsModule } from './posts/posts.module';
+import { PostService } from './post/post.service';
 @Module({
   imports: [PrismaModule, ConfigModule.forRoot({ isGlobal: true }), AuthModule, PostsModule],
+  providers: [PostService],
 })
 export class AppModule {}
